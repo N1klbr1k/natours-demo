9dd4eb86aba259cec1864d1a5d3df80e70c399d4
N1klbr1k <lemmonynikit@gmail.com>
2022-06-09 15:02:00 -0600

    implemented more stuff

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "Hello from the server side!", app: "Natours" });
-// });
+//---------------------------middleware-------------------------
+app.use(morgan("dev"));
+app.use(express.json());
 
-// app.post("/", (req, res) => {
-//   res.send("you can post to this endpoint...");
-// });
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
 
-app.get("/api/v1/tours");
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+//-------------------------------------------
 
+//-------------routes-------------------------------
+
+// app.get("/api/v1/tours", getAllTours);
+// app.post("/api/v1/tours", createNewTour);
+// app.get("/api/v1/tours/:id", getTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
+//--------------------------------------------------
+
+app.use("api/v1/users", userRouter);
+app.use("api/v1/tours", tourRouter);
+//-----------------start server-----------------------
 const PORT = 3000;
 
 app.listen(PORT, () => {
